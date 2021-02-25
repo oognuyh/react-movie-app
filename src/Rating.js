bae0c33379a838c6200e51832c9c30d3c5449d5a
Hyungoo Kang <oognuyh@gmail.com>
2021-02-25 23:16:56 +0900

    feat: Add a star rating

---
+++ src/Rating.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const Rating = ({ rating, ...rest }) => {
+  return (
+    <Wrapper {...rest}>
+      <Stars rating={rating}>
+        <span></span>
+      </Stars>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Stars = styled.div`
+  align-self: center;
+  display: inline-block;
+  height: 19px;
+  width: 108px;
+  overflow: hidden;
+  background: url(star.png) no-repeat;
+
+  span {
+    display: inline-block;
+    height: 19px;
+    overflow: hidden;
+    background: url(star.png) no-repeat;
+    background-position: left bottom;
+    line-height: 0;
+    vertical-align: top;
+    width: ${(props) => props.rating * 10}%;
+  }
+`;
+
+export default Rating;
