ea884839a5d37dafcd952a2145dc460dee8e36fc
Hyungoo Kang <oognuyh@gmail.com>
2021-03-03 22:20:34 +0900

    style: Fix typo in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import SearchIcon from "@material-ui/icons/Search";
 import styled from "styled-components";
 import Loader from "./Loader";
 import Movie from "./Movie";
@@ -8,9 +9,12 @@ import Movie from "./Movie";
 class App extends React.Component {
   state = {
     isLoading: true,
-    moveis: [],
+    isFiltering: false,
+    filteredMovies: [],
+    movies: [],
     hasMore: true,
     pageNo: 1,
+    userInput: "",
   };
 
   movieCount = 0;
@@ -28,7 +32,7 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    if (this.state.moveis.length >= this.movieCount) {
+    if (this.state.movies.length >= this.movieCount) {
       this.setState({ hasMore: false });
       return;
     }
@@ -58,17 +62,62 @@ class App extends React.Component {
     });
   };
 
+  handleChange = (e) => {
+    this.setState({ userInput: e.target.value });
+
+    if (this.state.userInput.length < 2) {
+      this.setState({ isFiltering: false, isLoading: false });
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ isLoading: true });
+    this.searchMovies();
+  };
+
+  searchMovies = async () => {
+    const { userInput } = this.state;
+
+    if (userInput.length > 0) {
+      this.setState({ isFiltering: true });
+
+      const {
+        data: {
+          data: { movies },
+        },
+      } = await axios.get(`https://yts-proxy.now.sh/list_movies.json?query_term=${userInput}`);
+
+      this.setState({ isLoading: false, isFiltering: true, filteredMovies: movies });
+    } else {
+      this.setState({ isFiltering: false, isLoading: false });
+    }
+  };
+
   componentDidMount() {
     this.init();
   }
 
   render() {
-    const { isLoading, movies, hasMore } = this.state;
+    const { isLoading, movies, hasMore, isFiltering, filteredMovies } = this.state;
 
     return (
       <Fragment>
+        <Header>
+          <SearchIcon />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter movie title, actor name or director name."
+              value={this.state.userInput}
+              onChange={this.handleChange}
+            />
+          </form>
+        </Header>
         {isLoading ? (
           <Loader />
+        ) : isFiltering ? (
+          <Grid>{this.renderMovies({ movies: filteredMovies })}</Grid>
         ) : (
           <InfiniteScroll dataLength={movies.length} next={this.getMovies} hasMore={hasMore} loader={<Loader height={"100px"} />}>
             <Grid>{this.renderMovies({ movies })}</Grid>
@@ -93,6 +142,30 @@ const Grid = styled.div`
 
   @media only screen and (max-width: 848px) {
     grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  padding: 20px 0;
+  color: white;
+
+  input {
+    height: 100%;
+    width: 60vw;
+    padding: 10px;
+    border: none;
+    background-color: none;
+    border-radius: 6px;
+    outline: none;
+  }
+
+  .MuiSvgIcon-root {
+    margin-right: 15px;
   }
 `;
 
